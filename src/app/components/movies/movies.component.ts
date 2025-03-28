3fa958ff01de7f1f0da457176c3c828060b1990a
Tanguy RAGUENES <tanguy.raguenes2024@campus-eni.fr>
2025-03-28 10:35:42 +0100

    Récupération films

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MoviesApiService } from '../../services/movies-api.service';
+import { Movie } from '../../interfaces/movie';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
@@ -11,8 +13,19 @@ export class MoviesComponent implements OnInit {
 
   private readonly moviesApiService:MoviesApiService=inject(MoviesApiService)
 
+  public moviesListToDisplay:Movie[]=[]
+
   ngOnInit(): void {
+
     this.moviesApiService.fetchData()
+
+    this.moviesApiService.moviesList.subscribe({
+      next: (value) => {
+        this.moviesListToDisplay = [...value];
+        value.forEach(element => console.log(element));
+      }
+    });
+
   }
 
 }
--- src/app/services/movies-api.service.ts
+++ src/app/services/movies-api.service.ts
@@ -1,28 +1,55 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable, OnInit } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
+import { BehaviorSubject } from 'rxjs';
+import { Movie } from '../interfaces/movie';
+import { TmdbApiResponse } from '../interfaces/tmdb-api-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesApiService {
 
+  public moviesList:BehaviorSubject<Movie[]>=new BehaviorSubject<Movie[]>([])
+
   private readonly http:HttpClient=inject(HttpClient)
+  private readonly url:string=environment.tmdbUrl
+  private readonly  headers = new HttpHeaders({
+    'Authorization': `Bearer ${environment.tmdbToken}`
+  })
 
   public fetchData(){
 
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${environment.tmdbToken}`
-    })
-
     console.log(environment.tmdbToken)
-    console.log(headers)
+    console.log(this.headers)
 
-    this.http.get<string>('https://api.themoviedb.org/3/authentication',{headers}).subscribe({
+    this.http.get<string>(`${this.url}authentication`
+      ,{headers:this.headers}).subscribe({
       next: (response) => console.log(response),
       error: (error) => console.error(error),
       complete: () => console.log('done')
     });
+
+
+    this.http.get<TmdbApiResponse>(`${this.url}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200`
+      ,{headers:this.headers}).subscribe({
+        next:(response)=>{
+          console.log(response)
+          this.UpdateMoviesList(response)
+        },
+        error: (error) => console.error(error),
+        complete: () => console.log('done')
+      })
   }
+
+  public UpdateMoviesList(response:TmdbApiResponse){
+
+    this.moviesList.next(response.results)
+
+
+  }
+
+
+
 
 }
